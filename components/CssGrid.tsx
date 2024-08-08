9553b1f4eb69ee22c3dafebddc5a557837ec63c0
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-08 08:25:34 +0700

    feat : add new box

--- components/CssGrid.tsx
+++ components/CssGrid.tsx
@@ -1,7 +1,29 @@
+import { data } from "@/app/data";
 import React from "react";
 
 const CssGrid = () => {
-  return <div>CssGrid</div>;
+  const boxStyle =
+    "border-2 rounded-xl p-6 flex flex-col hover:scale-105 shadow-xl bg-white overflow-hidden";
+
+  return (
+    <div className="grid md:grid-cols-4 auto-rows-[250px]  gap-2 my-2">
+      {data.map((item, i) => (
+        <div
+          key={i}
+          className={` ${i === 4 ? "md:col-span-2 md:row-span-2" : ""} ${
+            i === 0 ? "md:col-span-3 " : ""
+          } ${i === 1 ? "" : ""} ${boxStyle}`}
+        >
+          <img className={item.srcComp} src={item.src}></img>
+          <div>
+            <h2 className={item.headComp}>{item.head}</h2>
+            <p className={item.p1Comp}>{item.p1}</p>
+            <p className={item.p2Comp}>{item.p2}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CssGrid;
