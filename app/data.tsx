4b15e3ee4eee9b5b2bb16267798e16bc7bcf55d9
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-08 08:25:21 +0700

    feat : add new data

--- app/data.tsx
+++ app/data.tsx
@@ -1,1 +1,60 @@
-export const data = [];
+import { Component } from "react";
+
+export const data = [
+  {
+    headComp:
+      "font-mono font-bold tracking-wide animate-pulse line-clamp-5 md:text-7xl text-6xl mb-8",
+    head: "Hellooo 👋",
+    p1Comp:
+      "font-mono tracking tracking-wide line-clamp-6 md:text-4xl text-3xl ",
+    p1: "I'm Alfian Hanif FY",
+    p2Comp:
+      "font-mono tracking tracking-wide line-clamp-5 md:text-2xl text-1xl",
+    p2: "Teknik Informatika Ganesha",
+  },
+  {
+    srcComp: "rounded-lg h-[210px] w-[210px] ml-6",
+    src: "/photo.png",
+    alt: "foto diri",
+  },
+  {
+    headComp: "",
+    head: "Hellooo 👋",
+    p1Comp: "",
+    p1: "",
+    p2Comp: "",
+    p2: "",
+  },
+  {
+    headComp: "",
+    head: "Hellooo 👋",
+    p1Comp: "",
+    p1: "I'm Alfian Hanif FY",
+    p2Comp: "",
+    p2: "3",
+  },
+  {
+    headComp: "",
+    head: "Hellooo 👋",
+    p1Comp: "",
+    p1: "I'm Alfian Hanif FY",
+    p2Comp: "",
+    p2: "4",
+  },
+  {
+    headComp: "",
+    head: "Hellooo 👋",
+    p1Comp: "",
+    p1: "I'm Alfian Hanif FY",
+    p2Comp: "",
+    p2: "5",
+  },
+  {
+    headComp: "",
+    head: "Hellooo 👋",
+    p1Comp: "",
+    p1: "I'm Alfian Hanif FY",
+    p2Comp: "",
+    p2: "6",
+  },
+];
