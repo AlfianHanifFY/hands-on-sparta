8118bfc2f2d5d9cbba86792ec5824e8cd5a8849c
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-09 23:38:28 +0700

    feat L add ContactGrid component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 import CssGrid from "@/components/MainGrid";
 import MainGrid from "@/components/MainGrid";
 import SkillGrid from "@/components/SkillGrid";
+import ContactGrid from "@/components/ContactGrid";
 
 export default function Home() {
   return (
     <div className="p-10 bg-slate-100">
       <MainGrid />
       <SkillGrid />
+      <ContactGrid />
     </div>
   );
 }
