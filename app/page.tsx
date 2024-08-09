2e10f0f03c7873f2a288263055d92e48da84f453
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-09 19:53:39 +0700

    chore : rename component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
-import CssGrid from "@/components/CssGrid";
+import CssGrid from "@/components/MainGrid";
+import MainGrid from "@/components/MainGrid";
 
 export default function Home() {
   return (
     <div className="p-8">
-      <CssGrid />
+      <MainGrid />
     </div>
   );
 }
