250e10129b6dcd720a1bbde7731b2f1b7922ebaa
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-09 23:37:53 +0700

    feat : add contact info

---
+++ app/contact-data.tsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+
+export const data = [
+  {
+    headComp: "font-mono font-bold text-justify text-4xl",
+    head: "Contact Me ... ?",
+    imageComp: "",
+    image: "",
+    link: "",
+  },
+  {
+    headComp: "font-mono font-bold text-justify text-4xl",
+    head: "",
+    imageComp: "h-[50px] w-[50px] ml-7",
+    image: "/instagram.png",
+    link: "https://www.instagram.com/alfianhaniffy/",
+  },
+  {
+    headComp: "font-mono font-bold text-justify text-4xl",
+    head: "",
+    imageComp: "h-[50px] w-[50px] ml-7",
+    image: "/linkedin.png",
+    link: "https://www.linkedin.com/in/alfian-hanif-fy-b61669283/",
+  },
+  {
+    headComp: "font-mono font-bold text-justify text-4xl",
+    head: "",
+    imageComp: "h-[50px] w-[50px] ml-7",
+    image: "/github.png",
+    link: "https://github.com/AlfianHanifFY",
+  },
+  {
+    headComp: "font-mono font-bold text-justify text-4xl",
+    head: "",
+    imageComp: "h-[50px] w-[50px] ml-7",
+    image: "/twitter.png",
+    link: "https://x.com/AlfianHaniffy",
+  },
+];
