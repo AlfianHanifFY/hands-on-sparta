0a0405d93c6ec75ca514feec1d431c44e5772ff4
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-09 19:40:07 +0700

    feat : add more box

--- components/CssGrid.tsx
+++ components/CssGrid.tsx
@@ -3,22 +3,24 @@ import React from "react";
 
 const CssGrid = () => {
   const boxStyle =
-    "border-2 rounded-xl p-6 flex flex-col hover:scale-105 shadow-xl bg-white overflow-hidden";
+    "border-2 rounded-xl p-6 flex flex-col hover:scale-105 shadow-xl bg-white overflow-hidden ";
 
   return (
-    <div className="grid md:grid-cols-4 auto-rows-[250px]  gap-2 my-2">
+    <div className="grid md:grid-cols-4 auto-rows-[250px] gap-2 my-2">
       {data.map((item, i) => (
         <div
           key={i}
           className={` ${i === 4 ? "md:col-span-2 md:row-span-2" : ""} ${
             i === 0 ? "md:col-span-3 " : ""
-          } ${i === 1 ? "" : ""} ${boxStyle}`}
+          } ${i === 7 ? "md:col-span-4 h-[100px]" : ""} ${boxStyle}`}
         >
           <img className={item.srcComp} src={item.src}></img>
           <div>
             <h2 className={item.headComp}>{item.head}</h2>
             <p className={item.p1Comp}>{item.p1}</p>
             <p className={item.p2Comp}>{item.p2}</p>
+            <p className={item.p3Comp}>{item.p3}</p>
+            <p className={item.p4Comp}>{item.p4}</p>
           </div>
         </div>
       ))}
