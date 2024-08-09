32b4b6004b2ee4ed9fd98cb973775339480cda43
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-09 23:38:06 +0700

    feat : add contact component

---
+++ components/ContactGrid.tsx
@@ -0,0 +1,24 @@
+import { data } from "@/app/contact-data";
+import React from "react";
+
+const ContactGrid = () => {
+  const boxStyle =
+    "border-2 rounded-xl p-6 flex flex-col hover:bg-amber-200 hover:scale-105 shadow-xl bg-white overflow-hidden ";
+
+  return (
+    <div className="grid md:grid-cols-8 rows-[250px] gap-2 my-2">
+      {data.map((item, i) => (
+        <div key={i} className={`${i === 0 ? "col-span-4 " : ""} ${boxStyle}`}>
+          <div>
+            <h2 className={item.headComp}>{item.head}</h2>
+          </div>
+          <a href={item.link}>
+            <img className={item.imageComp} src={item.image}></img>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ContactGrid;
