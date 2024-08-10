c7131dc3539068651eda5435f8d8d4f527fda5b5
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-10 20:15:31 +0700

    chore: change web title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "web ku",
-  description: "aku buat web",
+  title: "Portfolio Alfian",
+  description: "web portfolio alfian",
 };
 
 export default function RootLayout({
